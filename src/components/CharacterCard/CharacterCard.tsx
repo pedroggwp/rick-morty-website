49f0725652d27358abbb6aa3923a6efcb79a81b7
pedroggwp <phsnnascimento@outlook.com>
2025-03-03 15:59:53 -0300

    design card

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,32 +1,44 @@
-import styles from './CharacterCard.module.css'
+import styles from "./CharacterCard.module.css";
 
 interface CharacterParams {
-    name: string,
-    specie: string,
-    gender: string,
-    status: string,
-    imageUrl: string
+  name: string,
+  species: string,
+  gender: string,
+  status: string,
+  imageUrl: string,
 }
 
-const CharacterCard: React.FC<CharacterParams> = ({name, specie, gender, status, imageUrl}) => {
+const CharacterCard: React.FC<CharacterParams> = ({
+  name,
+  species,
+  gender,
+  status,
+  imageUrl,
+}) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.imageContainer}>
         <img className={styles.img} src={imageUrl} alt="Imagem do personagem" />
       </div>
 
       <div className={styles.info}>
-        <h1>{name}</h1>
-        <h2>{specie}</h2>
+        <h1 className={styles.name}>{name}</h1>
+        <h2 className={styles.species}>{species}</h2>
 
-        <span>Gênero:</span>
-        <p>{gender}</p>
+        <div className={styles.details}>
+          <div className={styles.detailItem}>
+            <span className={styles.label}>Gênero:</span>
+            <span className={styles.value}>{gender}</span>
+          </div>
 
-        <span>Status:</span>
-        <p>{status}</p>
+          <div className={styles.detailItem}>
+            <span className={styles.label}>Status:</span>
+            <span className={styles.value}>{status}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default CharacterCard
+export default CharacterCard;
