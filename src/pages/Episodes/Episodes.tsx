a358d21c80802a2c0b271514cffcb01a49f7e030
laura4343 <123laurafarias@gmail.com>
2025-03-17 07:42:22 -0300

    location

--- src/pages/Episodes/Episodes.tsx
+++ src/pages/Episodes/Episodes.tsx
@@ -1,24 +1,23 @@
-// import Header from "../../components/Header/Header";
-// import OficialLogo from "../../assets/rick_morty_oficial_logo.svg";
-// import RickMortyPortal from "../../assets/rick-portal.png";
-// import styles from "./Episodes.module.css";
-// import { CharacterCard } from "../../components/CharacterCard/CharacterCard";
-// import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
-// import RickIcon from "../../assets/rick.svg"
-// import EpisodeIcon from "../../assets/episodes.svg"
-// import { InfoCard } from "../../components/InfoCard/InfoCard";
-// import PlacesIcon from "../../assets/locations.svg";
-// import { CHARACTER_PATH, PLACES_PATH, EPISODES_PATH } from "../../constants/paths";
-// import { Footer } from "../../components/Footer/Footer";
-
-import Search from "../../components/SearchBar/Search"
-import SearchButton from "../../components/SearchButton/SearchButton"
+import Search from "../../components/SearchBar/Search";
+import SearchButton from "../../components/SearchButton/SearchButton";
+import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
+import EpisodesIcon from "../../assets/episodes.svg";
+import { HOME_PATH } from "../../constants/paths";
 
 export default function Episodes() {
   return (
     <>
-      <Search/>
-      <SearchButton/>
+      <SectionTitle
+        title="EPISÓDIOS"
+        iconUrl={EpisodesIcon}
+        altIcon="Rick Morty ícone"
+        navigateTo={HOME_PATH}
+        arrowSide="left"
+        description="Pesquise um episódio por sua numeração ou nome."
+      />
+
+      <Search />
+      <SearchButton />
     </>
   );
 }
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,19 +1,15 @@
-
 import { useRoutes } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
-import { Episodes  } from "../pages/Episodes/Episodes"
+import Episodes from "../pages/Episodes/Episodes";
 import { Character } from "../pages/Character/Character";
 import { Locations } from "../pages/Locations/Locations";
 
 export const MainRoutes = () => {
-
   return useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/episodes", element: <Episodes   /> },
+    { path: "/episodes", element: <Episodes /> },
     { path: "/character", element: <Character /> },
     { path: "/locations", element: <Locations /> },
   ]);
 };
-}
-
