a91c1c296bf51cccb2ac26a5a39f02743fb7f318
pedroggwp <phsnnascimento@outlook.com>
2025-03-24 07:53:17 -0300

    feat: character page almost completed

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -3,11 +3,14 @@ import { HOME_PATH } from "../../constants/paths"
 import RickIcon from "../../assets/rick.svg"
 import { CharacterCard } from "../../components/CharacterCard/CharacterCard"
 import styles from "./Character.module.css"
+import { Selection } from "../../components/Selection/Selection"
+import Search from "../../components/SearchBar/Search"
+import { SearchButton } from "../../components/SearchButton/SearchButton"
 
 export const Character = () => {
 
     return (
-      <>
+      <div className={styles.characterContainer}>
         <SectionTitle
           title="PERSONAGENS"
           iconUrl={RickIcon}
@@ -16,10 +19,22 @@ export const Character = () => {
           arrowSide="left"
           description="Descrição"
         />
-
         <p>Ache personagens pelo nome, ou aplique filtros.</p>
 
         <span>Resultados (######)</span>
+
+        <div className={styles.selectionContainer}>
+          <Selection items={["OI", "A"]} />
+
+          <Selection items={["OI", "A"]} />
+
+          <Selection items={["OI", "A"]} />
+
+          <Search placeholder="Digite um personagem" />
+
+          <SearchButton onClick={() => console.log("clicou")}/>
+        </div>
+
         {/* CARREGAR CARDS DOS PERSONAGENS */}
 
         <div className={styles.cardsContainer}>
@@ -71,6 +86,6 @@ export const Character = () => {
             imageUrl="https://rickandmortyapi.com/api/character/avatar/385.jpeg"
           />
         </div>
-      </>
+      </div>
     );
 }
